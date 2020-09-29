c9f689dc784ff31a599ada4192a0927138d68480
Arihant Banthia <me.arihant.banthia@gmail.com>
2020-09-29 11:02:13 +0530

    Added URL for the BoothCreateListView in url.py in booth app

--- booth/urls.py
+++ booth/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
+
+from booth.views import booth
 from .views.index import index
 
 app_name = 'booth'
 urlpatterns = [
     path('', index, name='index'),
+    path('create-list-booth/',
+         booth.BoothCreateListView.as_view(),
+         name='create-list-booth')
 ]
