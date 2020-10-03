824a289349a92fdd83664761a76c98ce349dd38a
Arihant Banthia <me.arihant.banthia@gmail.com>
2020-10-03 08:26:46 +0530

    Removed the code as found diff solution

--- booth/templatetags/booth_template_tags.py
+++ booth/templatetags/booth_template_tags.py
@@ -4,10 +4,6 @@ from django.shortcuts import get_object_or_404
 from booth import models
 register = template.Library()
 
-@register.simple_tag('check_route')
+@register.simple_tag
 def check_route_no(route_no):
-    route = models.Route.objects.filter(route_no=route_no)
-    if route.count() == 1:
-        return route.xname
-    else:
-        return "Does Not Exists"
+    return route_no
