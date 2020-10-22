a7f74c9a024dc229d4da5a55dc60a52a4edfa2f2
Arihant Banthia <me.arihant.banthia@gmail.com>
2020-10-22 08:24:12 +0530

    change the form to form_class in RouteCreateListView

--- booth/views/route_view.py
+++ booth/views/route_view.py
@@ -6,12 +6,12 @@ from booth import models
 from booth.forms import route_form
 
 class RouteCreateListView(CreateView):
-    form = route_form.RouteCreateForm
+    form_class = route_form.RouteCreateForm
     model = models.Route
     template_name = 'booth/route_create.html'
 
     def get_context_data(self, **kwargs):
-        context = super(RouteCreateListView, self).get_context_data(**kwargs)
-        context['route'] = models.Route.objects.all()
+        context = super().get_context_data(**kwargs)
+        context['routes'] = models.Route.objects.all()
         return context
 
